e2e4fa922fea6a2d5046cf4de3c75275f30278fc
Валентин Борнашов <shipverty@vk.com>
2023-03-28 15:53:14 +0700

    feature(MasterTeacherApp): созданы подпапки в решении и добавлена иконка

--- src/MasterTeacherApp/MasterTeacherApp/View/MainForm.Designer.cs
+++ src/MasterTeacherApp/MasterTeacherApp/View/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace MasterTeacherApp
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.SuspendLayout();
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "MasterTeacherApp";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
