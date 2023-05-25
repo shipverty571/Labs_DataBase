864b242dbeffb5fa9f7c9bf023b28b705675d68c
Валентин Борнашов <shipverty@vk.com>
2023-05-26 00:52:17 +0700

    я сделал лабу спасибо

---
+++ src/MasterTeacherApp/MasterTeacherApp/View/SQLForm.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace MasterTeacherApp.View
+{
+    public partial class SQLForm : Form
+    {
+        public SQLForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
